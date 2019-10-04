cef8da5f85822db7838bf8003d69eea8220feb1f
amelie <ameliebarthel@hotmail.fr>
2019-10-04 22:22:37 +0200

    first push

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1;$i <= 10;$i++)
+        {
+            $article = new Article();
+            $article->setTitle("Titre de l'article $i");
+            $article->setContent("<p>Contenu de l'article $i</p>");
+            $article->setImage("http://placehold.it/350x150");
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+
+        }
+        $manager->flush();
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
